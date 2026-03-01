e1e12e4407ded81d4daf3501a62b3be21ab0a006
Andyvucoder <anhvuktnh@gmail.com>
2026-03-01 22:20:16 +0700

    Update

--- test-api.ps1
+++
@@ -1,2 +0,0 @@
-$body = @{ companyName='Test Corp'; adminEmail='admin@testcorp.com'; password='securepass'; domain='testcorp.com' } | ConvertTo-Json
-Invoke-RestMethod -Uri 'http://localhost:8080/api/tenants/onboard' -Method Post -Body $body -ContentType 'application/json'
